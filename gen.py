f1063cf788fcb31d53019a45839debd27ff0bc12
Michael Munch <mm.munk@gmail.com>
2020-05-18 14:14:44 +0200

    Switched to python generate Dockerfiles

--- gen.py
+++ gen.py
@@ -1,3 +1,7 @@
+from string import Template
+import os
+
+temp = Template("""
 FROM ubuntu:bionic
 
 RUN apt-get update -y \
@@ -14,7 +18,7 @@ RUN apt-get update -y \
 
 ARG INSTALL_PREFIX=/usr/local/cross
 ARG TARGET=riscv-elf
-ENV PATH "$INSTALL_PREFIX/bin:$PATH"
+ENV PATH "$$INSTALL_PREFIX/bin:$$PATH"
 
 RUN apt-get install -y wget curl git
 
@@ -24,22 +28,22 @@ RUN curl -L https://github.com/bminor/binutils-gdb/archive/master.tar.gz \
     | tar --strip-components 1 -xzC ./binutils-gdb
 RUN mkdir build-binutils \
     && cd build-binutils \
-    && ../binutils-gdb/configure --target=$TARGET --prefix="$INSTALL_PREFIX" --with-sysroot --disable-nls --disable-werror \
+    && ../binutils-gdb/configure --target=$$TARGET --prefix="$$INSTALL_PREFIX" --with-sysroot --disable-nls --disable-werror \
     && make \
     && make install
 
 
 RUN mkdir gcc
-RUN curl -L https://github.com/gcc-mirror/gcc/archive/master.tar.gz \
+RUN curl -L ${gcc_url} \
     | tar --strip-components 1 -xzC ./gcc
 
 RUN apt-get install -y gcc-multilib
 
-RUN which -- $TARGET-as || echo $TARGET-as is not in the PATH
+RUN which -- $$TARGET-as || echo $$TARGET-as is not in the PATH
  
 RUN mkdir build-gcc \
     && cd build-gcc \
-    && ../gcc/configure --target=$TARGET --prefix="$INSTALL_PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+    && ../gcc/configure --target=$$TARGET --prefix="$$INSTALL_PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
 WORKDIR /tmp/build-gcc    
 RUN make all-gcc -j8
 RUN  make all-target-libgcc 
@@ -64,8 +68,33 @@ RUN apt-get update -y \
 ARG INSTALL_PREFIX=/usr/local/cross
 ARG TARGET=riscv-elf
 
-ENV PATH "$INSTALL_PREFIX/bin:$PATH"
-ENV CROSS_COMPILE ${TARGET}-
+ENV PATH "$$INSTALL_PREFIX/bin:$$PATH"
+ENV CROSS_COMPILE $${TARGET}-
 
-RUN mkdir -p $INSTALL_PREFIX
-COPY --from=0 $INSTALL_PREFIX $INSTALL_PREFIX
+RUN mkdir -p $$INSTALL_PREFIX
+COPY --from=0 $$INSTALL_PREFIX $$INSTALL_PREFIX
+""")
+
+vers = [
+    "8.1.0",
+    "8.2.0",
+    "8.3.0",
+    "8.4.0",
+    "9.1.0",    
+    "9.2.0",
+    "9.3.0",
+    "10.1.0"
+]
+
+def output_docker_file(d, gcc_url):
+    os.makedirs(d, exist_ok=True)
+    with open("{}/Dockerfile".format(d), "w") as f:
+        s = temp.substitute(gcc_url=gcc_url)
+        f.write(s)
+
+for v in vers:
+    url = "https://github.com/gcc-mirror/gcc/archive/releases/gcc-{}.tar.gz".format(v)
+    output_docker_file(v, url)
+
+
+
